a5ce02a4bae2562541305cec22d1ee66369c8ebc
Rikard Löwbeer <118805329+LowbeerR@users.noreply.github.com>
2023-02-06 13:14:33 +0100

    Update hanoi.ex

--- Tornen i Hanoi/hanoi.ex
+++ Tornen i Hanoi/hanoi.ex
@@ -1,16 +1,12 @@
 defmodule Hanoi do
 
-  def hanoi(0, _, _, _) do nil end
-  def hanoi(1, a, _, c) do {:move, a, c} end
-  def hanoi(2, a, b, c) do
-    [{:move,a , b}, {:move, a, c}, {:move, b, c}]
-  end
+  def hanoi(0, _, _, _) do [] end
 
   def hanoi(n, a, b, c) do
     #move tower of size n-1
     hanoi(n-1, a, c, b) ++
     #[ move one disc ... ]
-    [hanoi(1, a, b, c)] ++
+    [{:move, a, c}] ++
     # move tower of size n-1
     hanoi(n-1, b, a, c)
    end
